9e7a41ba141c450e1d8fecad71f3013c70645f2c
Binu Jassim <ubjassim@gmail.com>
2017-09-05 12:42:44 -0700

    Update blog.js

--- blog.js
+++ blog.js
@@ -35,7 +35,7 @@ gotorecipes = function () {
 }
 
 gotoreviews = function() {
-	jQuery(location).attr('href', 'index.html')
+	jQuery(location).attr('href', 'http://bonapnotsopetite.com')
 }
 
 setup = function() {
